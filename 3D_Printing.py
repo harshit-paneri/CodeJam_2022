7aca119e31cf5e3cc73f6238497b3cac3a75d40a
Harshit Paneri <82382478+harshit-paneri@users.noreply.github.com>
2022-04-02 05:56:18 +0530

    3D_Printing codejam
    3D_Printing codejam 2022

---
+++ 3D_Printing.py
@@ -0,0 +1,44 @@
+import numpy as np
+n=int(input())
+for _ in range(n):
+    p1=list(map(int,input().split()))
+    p2=list(map(int,input().split()))
+    p3=list(map(int,input().split()))
+    
+    printers=[p1,p2,p3]
+    printers=np.array(printers)
+    printers=np.transpose(printers)
+    print("Case #",_+1,": ",sep='',end='')
+    
+    final_score=1000000
+    
+    printers_array=[min(printers[x]) for x in range(4)]
+    
+    # print(printers_array)
+        
+    for i in range(4):
+        final_score-=printers_array[i]
+    # print(final_score)
+        
+    if final_score>0:
+        print("IMPOSSIBLE")
+        
+    elif final_score==0:
+        for i in range(4):
+            print(printers_array[i],end=' ')
+        print()
+    
+    else:
+        available=sum(printers_array)+final_score
+        # print(available)
+        for i in range(4):
+            x=(min(available,printers_array[i]))
+            if x>=0:
+                print(x,end=' ')
+            else:
+                print(0,end=' ')
+            available-=printers_array[i]
+        print()
+        # printers[3]+=final_score
+        # for i in range(4):
+        #     print(printers_array[i],end=' ')
